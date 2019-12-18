2f65b3c0b86afa99d506e555ae44437719df280b
cic-odoo <cic@odoo.com>
2019-12-18 13:57:26 -0800

    [IMP] Finish the scheduled action

--- luonto_product/models/product.py
+++ luonto_product/models/product.py
@@ -16,7 +16,8 @@ class ProductTemplate(models.Model):
 
     @api.multi
     def get_attr_no_buy(self):
-        return self.attribute_line_ids.mapped('product_template_value_ids').filtered(lambda a: a.product_attribute_value_id.is_not_buy).ids
+        return self.attribute_line_ids.mapped('product_template_value_ids').filtered(
+            lambda a: a.product_attribute_value_id.is_not_buy).ids
 
 
 class ProductAttributeValue(models.Model):
@@ -30,102 +31,120 @@ class ProductAttributeValue(models.Model):
         string="Sub Attribute Values")
 
     is_not_buy = fields.Boolean(string="Is Not Buyable",
-                               help="Check this box to restrict customer from selecting and "
-                                    "buying this product with this attribute.")
+                                help="Check this box to restrict customer from selecting and "
+                                     "buying this product with this attribute.")
 
     @api.multi
     def write(self, values):
         res = super(ProductAttributeValue, self).write(values)
 
         print("WE GOT TO WRITE HERE")
-        # # All attribute value selected(ex:cotton)
-        # for r in self.filtered(lambda r: r.attribute_value_ids):
-        #     tmpl_attr = {}
-        #     # create a dict of attributes+values for product.template
-        #     for sub in r.attribute_value_ids:
-        #         tmpl_attr.setdefault(sub.attribute_id.id, []).append(sub.id)
-        #
-        #     # All attribute lines with current attribute value(r)
-        #     attr_lines = self.env['product.template.attribute.line'].search([['value_ids', 'in', r.id]])
-        #     # prefetching exclusion env
-        #     combination_exclude = self.env['product.template.attribute.exclusion']
-        #
-        #     # Add the sub attr val to the current attribute value(r)
-        #     for line in attr_lines:
-        #         prod = line.product_tmpl_id
-        #         for key in tmpl_attr.keys():
-        #             exist = [v for v in prod.attribute_line_ids if key == v.attribute_id.id]
-        #             if exist:
-        #                 for sub_id in tmpl_attr[key]:
-        #                     exist[0].write({'value_ids': [(4, sub_id, 0)]})
-        #             else:
-        #                 val = [(4, s, 0) for s in tmpl_attr[key]]
-        #                 prod.write({
-        #                     'attribute_line_ids': [(0, 0, {
-        #                         'product_tmpl_id': prod.id,
-        #                         'attribute_id': key,
-        #                         'value_ids': val,
-        #                     })]
-        #                 })
-        #         prod.create_variant_ids()
-        #
-        #         # product.template.attribute.value on current product.template that ARE attribute(fabric) and NOT current attribute(poly) > should only be cotton
-        #         need_exclusion = self.env['product.template.attribute.value'].search([
-        #             ['product_tmpl_id', '=', prod.id],
-        #             ['attribute_id', '=', r.attribute_id.id],
-        #             ['product_attribute_value_id', '!=', r.id]])
-        #         # product.template.attribute.value on current product.template that are of the same attribute as those in the sub attributes.
-        #         possible_exclusion = self.env['product.template.attribute.value'].search([
-        #             ['product_tmpl_id', '=', prod.id],
-        #             ['attribute_id', 'in', list(tmpl_attr.keys())]])
-        #
-        #         # product.template.attribute.values which need exclusion
-        #         for need in need_exclusion:
-        #             # product.template.attribute.values that might need to be excluded
-        #             for ex in possible_exclusion:
-        #                 # if not sub attributes of the need
-        #                 if ex.product_attribute_value_id not in need.product_attribute_value_id.attribute_value_ids:
-        #                     has_tmpl_line = [t for t in need.exclude_for if
-        #                                      t.product_tmpl_id == need.product_tmpl_id]
-        #                     if has_tmpl_line:
-        #                         need.write({
-        #                             'exclude_for': [(1, has_tmpl_line[0].id, {
-        #                                 'product_tmpl_id': need.product_tmpl_id.id,
-        #                                 'value_ids': [(4, ex.id, 0)]
-        #                             })]
-        #                         })
-        #                     # create exclusion line with current template
-        #                     else:
-        #                         need.write({
-        #                             'exclude_for': [(0, 0, {
-        #                                 'product_tmpl_id': need.product_tmpl_id.id,
-        #                                 'value_ids': [(4, ex.id, 0)]
-        #                             })]
-        #                         })
-        #                     # Handle reverse exclusion
-        #                     rev_has_tmpl_line = [t for t in ex.exclude_for if
-        #                                          t.product_tmpl_id == ex.product_tmpl_id]
-        #                     if rev_has_tmpl_line:
-        #                         ex.write({
-        #                             'exclude_for': [(1, rev_has_tmpl_line[0].id, {
-        #                                 'product_tmpl_id': ex.product_tmpl_id.id,
-        #                                 'value_ids': [(4, need.id, 0)]
-        #                             })]
-        #                         })
-        #                     else:
-        #                         ex.write({
-        #                             'exclude_for': [(0, 0, {
-        #                                 'product_tmpl_id': ex.product_tmpl_id.id,
-        #                                 'value_ids': [(4, need.id, 0)]
-        #                             })]
-        #                         })
-        #         # Set exclusion boolean on the product variant if attribute values align
-        #         cur_var = prod.product_variant_ids
-        #         for var in cur_var:
-        #             values_ids = var.product_template_attribute_value_ids
-        #             domain = [('product_template_attribute_value_id', 'in', values_ids.ids),
-        #                       ('value_ids', 'in', values_ids.ids), ('product_tmpl_id', '=', prod.id)]
-        #             is_ex = combination_exclude.search(domain)
-        #             if is_ex:
-        #                 var.write({'is_exclude': True})
-        # return res
+        # All attribute value selected(ex:cotton)
+        # Filtered by if it has attribute_value_ids(sub attr)
+        for main_attr in self.filtered(lambda a: a.attribute_value_ids):
+            tmpl_attr = {}
+            # create a dict of attributes+values for product.template
+            # ex: {color.id: [blue.id, black.id]}
+            for sub_attr in main_attr.attribute_value_ids:
+                tmpl_attr.setdefault(sub_attr.attribute_id.id, []).append(sub_attr.id)
+
+            # All attribute lines with current attribute value(main_attr)
+            # lines are on product.template to generate product.template.attribute.value
+            attr_lines = self.env['product.template.attribute.line'].search([['value_ids', 'in', main_attr.id]])
+            # prefetching exclusion env for later
+            combination_exclude = self.env['product.template.attribute.exclusion']
+
+            # Add the sub attr val to the current attribute value(main_attr)
+            # for line all attr lines on the a product template
+            for line in attr_lines:
+                # line will have unique prod.temp associated (ex: couch)
+                prod = line.product_tmpl_id
+                # add sub attr val to attr lines IF the line for that attribute exists
+                # for attr_id(ex: color) in attributes
+                for attr_id in tmpl_attr.keys():
+                    # check if the line exists on the prod with the current attribute(ex: color)
+                    exist = [v for v in prod.attribute_line_ids if attr_id == v.attribute_id.id]
+                    if exist:
+                        # add the sub attr vals (ex: black, blue) to the existing attr line
+                        for sub_id in tmpl_attr[attr_id]:
+                            exist[0].write({'value_ids': [(4, sub_id, 0)]})
+                    else:
+                        # otherwise create a new line with the attr_id(ex: color)
+                        val = [(4, s, 0) for s in tmpl_attr[attr_id]]
+                        # then add the sub attr vals (ex: black, blue)
+                        prod.write({
+                            'attribute_line_ids': [(0, 0, {
+                                'product_tmpl_id': prod.id,
+                                'attribute_id': attr_id,
+                                'value_ids': val,
+                            })]
+                        })
+                # create the variants(prod.prod) for that prod.temp manually
+                prod.create_variant_ids()
+
+                # Use of product.template.attribute.value:
+                # Materialized relationship between attribute values
+                # and product template generated by the product.template.attribute.line
+
+                # product.template.attribute.value on current product.template that
+                # ARE attribute(fabric) and NOT current attribute(poly) > should only be cotton
+                need_exclusion = self.env['product.template.attribute.value'].search([
+                    ['product_tmpl_id', '=', prod.id],
+                    ['attribute_id', '=', main_attr.attribute_id.id],
+                    ['product_attribute_value_id', '!=', main_attr.id]])
+                # product.template.attribute.value on current product.template that
+                # ARE of the same attribute as those in the sub attributes.
+                possible_exclusion = self.env['product.template.attribute.value'].search([
+                    ['product_tmpl_id', '=', prod.id],
+                    ['attribute_id', 'in', list(tmpl_attr.keys())]])
+
+                # product.template.attribute.values which need exclusion
+                for need in need_exclusion:
+                    # product.template.attribute.values that might need to be excluded
+                    for possible in possible_exclusion:
+                        # if not sub attributes of the need
+                        if possible.product_attribute_value_id not in need.product_attribute_value_id.attribute_value_ids:
+                            has_tmpl_line = [t for t in need.exclude_for if
+                                             t.product_tmpl_id == need.product_tmpl_id]
+                            if has_tmpl_line:
+                                need.write({
+                                    'exclude_for': [(1, has_tmpl_line[0].id, {
+                                        'product_tmpl_id': need.product_tmpl_id.id,
+                                        'value_ids': [(4, possible.id, 0)]
+                                    })]
+                                })
+                            # create exclusion line with current template
+                            else:
+                                need.write({
+                                    'exclude_for': [(0, 0, {
+                                        'product_tmpl_id': need.product_tmpl_id.id,
+                                        'value_ids': [(4, possible.id, 0)]
+                                    })]
+                                })
+                            # Handle reverse exclusion
+                            rev_has_tmpl_line = [t for t in possible.exclude_for if
+                                                 t.product_tmpl_id == possible.product_tmpl_id]
+                            if rev_has_tmpl_line:
+                                possible.write({
+                                    'exclude_for': [(1, rev_has_tmpl_line[0].id, {
+                                        'product_tmpl_id': possible.product_tmpl_id.id,
+                                        'value_ids': [(4, need.id, 0)]
+                                    })]
+                                })
+                            else:
+                                possible.write({
+                                    'exclude_for': [(0, 0, {
+                                        'product_tmpl_id': possible.product_tmpl_id.id,
+                                        'value_ids': [(4, need.id, 0)]
+                                    })]
+                                })
+                # Set exclusion boolean on the product variant if attribute values align
+                cur_var = prod.product_variant_ids
+                for var in cur_var:
+                    values_ids = var.product_template_attribute_value_ids
+                    domain = [('product_template_attribute_value_id', 'in', values_ids.ids),
+                              ('value_ids', 'in', values_ids.ids), ('product_tmpl_id', '=', prod.id)]
+                    is_ex = combination_exclude.search(domain)
+                    if is_ex:
+                        var.write({'is_exclude': True})
+        return res
