1dbdafb2e1fd2d3cde167366add3653e27cf6bf2
cic-odoo <cic@odoo.com>
2020-03-26 11:18:40 -0700

    [FIX] REM accidentally added module

--- luonto_credit_limit/models/stock.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, api, fields
-
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    is_credit_limit_exceeded = fields.Boolean(related="partner_id.is_credit_limit_exceeded")
