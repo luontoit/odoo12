d54dbdf201897dd7803a1d3803d3a61623adb303
Zeno Nanon <zna@odoo.com>
2020-08-10 15:10:38 -0700

    [DEL] Removed duplicate module

--- luonto_credit_limit/luonto_account_bill/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import account_move
-from . import customer_type
-from . import partner
--- luonto_credit_limit/luonto_account_bill/models/customer_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api, fields
-
-class CustomerType(models.Model):
-    _name="luonto_account_bill.customer_type"
-    _description="Customer Type"
-
-    name=fields.Char(string="Name")
-    commission=fields.Float(string="Commission Percentage", widget="percentage")
